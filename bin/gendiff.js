62b9b34531a09f961f015fcea3ed2e275ec9863b
m1chendi <m1chendi.0@gmail.com>
2023-05-05 13:45:59 +0300

    changes makefile, package.json, gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-V, --version', 'output the version number')
+  .option('-h, --help', 'display help for command')
+  .action((filepath1, filepath2, options) => {
+    
+  })
