5fab202043980c5de1e2e988a5cd30f872699f47
m1chendi <m1chendi.0@gmail.com>
2023-09-12 16:15:12 +0300

    commit5

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,11 +3,8 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'display help for command')
+  .version('1.0.0');
 
-program.parse();
+program.parse(process.argv);
 
