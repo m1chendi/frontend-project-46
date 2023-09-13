847170847ef1e2c0412de42b8991333fe3c2c20b
m1chendi <m1chendi.0@gmail.com>
2023-09-13 17:54:49 +0300

    commit7

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,16 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const options = program.opts();
+    console.log(gendiff(filepath1, filepath2, options.format));
+  });
 
 program.parse(process.argv);
 
