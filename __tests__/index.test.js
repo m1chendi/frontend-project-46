18992f266313c1bcd5dce58915bc3c6f50c6ed4e
m1chendi <m1chendi.0@gmail.com>
2023-09-19 14:27:54 +0300

    commit2

---
+++ __tests__/index.test.js
@@ -0,0 +1,7 @@
+import gendiff from '../src/index.js';
+
+import resultStylish from '../__fixtures__/result.js';
+
+test('json', () => {
+  expect(gendiff('/__fixtures__/file1.json', '/__fixtures__/file2.json')).toBe(resultStylish);
+});
