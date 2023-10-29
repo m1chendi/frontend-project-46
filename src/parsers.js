360a393b88b883fd1bd26283134a4167f01835b0
m1chendi <m1chendi.0@gmail.com>
2023-10-30 01:20:43 +0300

    commit

--- src/parsers.js
+++ src/parsers.js
@@ -1,19 +1,9 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import yaml from 'js-yaml';
-import path from 'node:path';
-import fs from 'fs';
 
-const getExtension = (filename) => path.extname(filename).slice(1);
-
-const getName = (filename) => path.basename(filename);
-
-const parsesFile = (filename) => {
-  const name = getName(filename);
-  const extension = getExtension(name);
-
-  return extension !== '.json'
-    ? yaml.load(fs.readFileSync(filename, 'utf-8'))
-    : JSON.parse(fs.readFileSync(filename, 'utf-8'));
+const parse = {
+  json: JSON.parse,
+  yaml: yaml.load,
+  yml: yaml.load,
 };
 
-export default parsesFile;
+export default (data, format) => parse[format](data);
